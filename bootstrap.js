a07b8359c7289f66698c25281ded477625e8e0b0
Nobody <xposemarket@gmail.com>
2026-08-17 01:23:22 -0400

    Add Flight Universe bootstrap loader

---
+++ bootstrap.js
@@ -0,0 +1,14 @@
+const PARTS = [
+  './app-part-00.txt',
+  './app-part-01.txt',
+  './app-part-02.txt',
+  './app-part-03.txt',
+  './app-part-04.txt'
+];
+const responses = await Promise.all(PARTS.map((url) => fetch(url)));
+for (const response of responses) {
+  if (!response.ok) throw new Error(`Failed to load ${response.url}: ${response.status}`);
+}
+const source = (await Promise.all(responses.map((response) => response.text()))).join('');
+const moduleUrl = URL.createObjectURL(new Blob([source], { type: 'text/javascript' }));
+try { await import(moduleUrl); } finally { URL.revokeObjectURL(moduleUrl); }
